a24b8dc123896f2d4fe6ca3c8710d45a96415032
Alexander Tikhonov <at1385@yandex.ru>
2022-07-25 09:45:35 +0500

    feat: add 07-conditions-n-loops-tasks/getFizzBuzz implementation

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -27,8 +27,20 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (!(num % 3) && !(num % 5)) {
+    return 'FizzBuzz';
+  }
+
+  if (!(num % 3)) {
+    return 'Fizz';
+  }
+
+  if (!(num % 5)) {
+    return 'Buzz';
+  }
+
+  return num;
 }
 
 
